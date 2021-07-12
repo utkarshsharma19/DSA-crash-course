5aa67df38d367c84f31e07c80f25fc9562ffa036
utkarshsharma19 <72225644+utkarshsharma19@users.noreply.github.com>
2021-07-12 14:59:37 +0530

    Create 33. Search in Rotated Sorted Array.java

---
+++ 33. Search in Rotated Sorted Array.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+        while(low<=high){
+            int mid = (low+high)/2;
+            if(nums[mid]==target){
+                return mid;
+            }
+            else if(nums[mid]>=nums[low]){
+                if(nums[mid]>=target && nums[low]<=target){
+                    {
+                        high = mid-1; 
+                    } 
+                }
+                    else{
+                    low = mid+1;
+                    }
+                    
+                
+            }
+                else
+                {
+                    if(nums[mid]<=target && nums[high]>=target){
+                        low = mid+1;
+                    }
+                    else
+                    {
+                        high = mid-1;
+                    }
+                }
+            }
+         return -1;
+        }
+       
+    }
